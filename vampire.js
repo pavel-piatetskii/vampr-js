68b9f6fac692374ca2135110561b7c2e0ae23bc4
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-13 23:51:56 +0000

    Intro task

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+  if (this.creator) {
+    return this.creator.numberOfVampiresFromOriginal + 1;
+  }
+  return 0;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,6 +43,21 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    
+    const ancestors = function(vamp) {
+      const output = { [vamp.name] : vamp };
+      while (vamp.creator) {
+        output[vamp.creator.name] = vamp.creator;
+        vamp = vamp.creator;
+      }
+      return output;
+    }
+
+    const ancest1 = ancestors(this);
+    const ancest2 = ancestors(vampire);
+    for (const name of Object.keys(ancest1)) {
+      if (Object.keys(ancest2).includes(name)) return ancest1[name]
+    }
 
   }
 }
